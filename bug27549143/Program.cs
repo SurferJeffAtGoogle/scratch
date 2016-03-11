d1ee8a2ef82afc1447f4b1fdd5e3ec3535f285d4
Jeffrey Rennie <rennie@google.com>
2016-03-11 09:53:31 -0800

    code-formatter and codemaid.

--- bug27549143/Program.cs
+++ bug27549143/Program.cs
@@ -1,16 +1,26 @@
-﻿using Google.Apis.Datastore.v1beta2;
+﻿// Copyright(c) 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using Google.Apis.Datastore.v1beta2;
 using Google.Apis.Datastore.v1beta2.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace bug27549143
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var projectId = args[1];
             // Use Application Default Credentials.
