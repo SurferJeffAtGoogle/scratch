3e35bde022c7052e54d14c36d22b49119ebbd26e
Jeffrey Rennie <rennie@google.com>
2016-03-23 16:58:49 -0700

    Joins are hard.

--- NaiveJoin/NaiveJoinLib/Class1.cs
+++ NaiveJoin/NaiveJoinLib/Class1.cs
@@ -16,6 +16,7 @@ using Google.Apis.Datastore.v1beta2;
 using Google.Apis.Datastore.v1beta2.Data;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,13 +31,24 @@ namespace GoogleCloudSamples
         {
             _datastore = datastore;
         }
-        async Task<RunQueryResponse> ExecuteAsync(string datasetId, IEnumerable<RunQueryRequest> requests)
-        {            
-            var tasks = new Dictionary<Task<RunQueryResponse>, RunQueryRequest>();
-            // TODO: Use some kind of bit or flag value instead of int.  Otherwise,
-            // cursor boundaries can cause bugs.
-            var entityCounts = new Dictionary<Key, int>();
 
+        async Task<LookupResponse> ExecuteAsync(string datasetId, RunQueryRequest[] requests)
+        {            
+            var requestIndices = new Dictionary<Task<RunQueryResponse>, int>();
+            // Maps an entity key to the requests that found it.
+            // The value is a bitmap of indices of requests that found the entity.
+            var foundFlags = new Dictionary<Key, int>();
+            Debug.Assert(requests.Count() < 8 * sizeof(int) - 1,
+                $"Really?  You want to join more than {sizeof(int) - 1} queries?");
+            int foundAll = (1 << requests.Count()) - 1;
+            int limit = 0;
+            QueryResultBatch resultBatch = new QueryResultBatch()
+            {
+                EntityResults = new List<EntityResult>()
+            };
+            var matchingKeys = new List<Key>(); 
+
+            int requestIndex = 0;
             foreach (RunQueryRequest request in requests)
             {
                 request.Query.Projection = new PropertyExpression[]
@@ -50,24 +62,35 @@ namespace GoogleCloudSamples
                     }
                 };
                 request.Query.Limit = _batchSize;
-                tasks[_datastore.Datasets.RunQuery(request, datasetId).ExecuteAsync()] = request;
+                requestIndices[_datastore.Datasets.RunQuery(request, datasetId).ExecuteAsync()] = requestIndex++;
             }
-            Task<RunQueryResponse> task = await Task.WhenAny(tasks.Keys);
+            Task<RunQueryResponse> task = await Task.WhenAny(requestIndices.Keys);
             var batch = task.Result.Batch;
-            var completeRequest = tasks[task];
-            tasks.Remove(task);
+            requestIndex = requestIndices[task];
+            requestIndices.Remove(task);
             foreach (var entity in batch.EntityResults)
             {
-                int count = 0;
-                entityCounts.TryGetValue(entity.Entity.Key, out count);
-                entityCounts[entity.Entity.Key] = ++count;
-                if (count == requests.Count())
-                    ; // yield result.
+                int flags = 0;
+                foundFlags.TryGetValue(entity.Entity.Key, out flags);
+                if (flags == foundAll)
+                    continue;  // Already found and added to the results
+                flags |= requestIndex;
+                if (flags == foundAll)
+                {
+                    matchingKeys.Add(entity.Entity.Key);
+                    if (matchingKeys.Count >= limit)
+                    {
+                        return await _datastore.Datasets.Lookup(new LookupRequest()
+                        {
+                            Keys = matchingKeys
+                        }, datasetId).ExecuteAsync();
+                    }
+                }
             }
             if (batch.EntityResults.Count() > 0)
             {
-                completeRequest.Query.StartCursor = batch.EndCursor;
-                tasks[_datastore.Datasets.RunQuery(completeRequest, datasetId).ExecuteAsync()] = completeRequest;
+                requests[requestIndex].Query.StartCursor = batch.EndCursor;
+                requestIndices[_datastore.Datasets.RunQuery(requests[requestIndex], datasetId).ExecuteAsync()] = requestIndex;
             }
         }
     }
