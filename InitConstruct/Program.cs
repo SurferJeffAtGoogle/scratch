0adf0a29bfca6717d49ff79b7b23378014037d60
Jeffrey Rennie <rennie@google.com>
2017-09-21 15:23:52 -0700

    In what order are public properties initialized?

---
+++ InitConstruct/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InitConstruct
+{
+    class C 
+    {
+        private string _s;
+        public string S
+        {
+            get { return _s;}
+            set 
+            { 
+                Console.WriteLine("Setting S to " + value);
+                _s  = value;
+            }
+        }
+        
+        public C(string s) { 
+            _s = s; 
+            Console.WriteLine("Constructing C with " + s);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new C("A") {
+                S = "B"
+            };
+            Console.WriteLine("c.S: " + c.S);
+        }
+    }
+}
