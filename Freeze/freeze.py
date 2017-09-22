e7970e69f1ebb0ae75256d10e529c9c835e9ebb9
Jeffrey Rennie <rennie@google.com>
2017-09-22 13:05:07 -0700

    django.template.exceptions.TemplateSyntaxError, Unclosed tag on line 1: 'if'. Looking for one of: elif, else, endif.

--- Freeze/freeze.py
+++ Freeze/freeze.py
@@ -1,8 +1,20 @@
 # Hello
 from django.template.base import DebugLexer
 import django.template.base as dtbase
+from django.conf import settings
+import django
 from pprint import pprint
 import sys
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    }
+]
+settings.configure(TEMPLATES=TEMPLATES)
+django.setup()    
+
 
 def main():
     s = ('<html>{% if test %}<h1>{{ varvalue }}</h1>{% endif %}\n'
@@ -12,8 +24,8 @@ def main():
         + '{% comment %}\n'
         + 'You need a new hairstyle.\n'
         + '{% endcomment %}\n')
-    path = sys.argv[1]
-    s = open(path, 'rb').read()
+    # path = sys.argv[1]
+    # s = open(path, 'rb').read()
     lexer = DebugLexer(s)
     tokens = lexer.tokenize()
     if False:
@@ -21,17 +33,25 @@ def main():
             pprint(vars(token))
             pprint(token.split_contents())
         print
-    open(path, 'wb').write(''.join([reconstruct(token) for token in tokens]))
+    if True: # with open(path, 'wb') as out:
+        for token in tokens:
+            text = reconstruct(token)
+            if token.token_type == dtbase.TOKEN_BLOCK:
+                t = dtbase.Template(text)
+                nodelist = t.nodelist
+            sys.stdout.write(text)
+        
 
 
+    
 def reconstruct(token):
-    functions = {
-        dtbase.TOKEN_TEXT: lambda token: token.contents,
-        dtbase.TOKEN_VAR: lambda token: '{{%s}}' % token.contents,
-        dtbase.TOKEN_BLOCK: lambda token: '{%% %s %%}' % token.contents,
-        dtbase.TOKEN_COMMENT: lambda token: '{# %s #}' % token.contents
-    }
-    return functions[token.token_type](token)
+    """Given a token, reconstruct the original text."""
+    # TOKEN_TEXT = 0
+    # TOKEN_VAR = 1
+    # TOKEN_BLOCK = 2
+    # TOKEN_COMMENT = 3
+    formats = ['%s', '{{%s}}', '{%% %s %%}', '{# %s #}']
+    return formats[token.token_type] % token.contents 
 
 if __name__ == '__main__':
     main()
