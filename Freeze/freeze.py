f4d154e640fedb7318a87349f85e4d3d71b4028d
Jeffrey Rennie <rennie@google.com>
2017-09-22 13:52:24 -0700

    Correctly disassembling and reconstructing include node.

--- Freeze/freeze.py
+++ Freeze/freeze.py
@@ -1,6 +1,19 @@
-# Hello
+# Copyright (c) 2016 Google Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 from django.template.base import DebugLexer
 import django.template.base as dtbase
+import django.template.engine as dtengine
 from django.conf import settings
 import django
 from pprint import pprint
@@ -18,7 +31,7 @@ django.setup()
 
 def main():
     s = ('<html>{% if test %}<h1>{{ varvalue }}</h1>{% endif %}\n'
-        + '{% include "another_template.html" with a="b" and c="d" %}'
+        + '{% include "another_template.html" with project="foo" file="/hello.cs" %}'
         + '</html>\n'
         + '{# cheesy #}\n'
         + '{% comment %}\n'
@@ -33,18 +46,27 @@ def main():
             pprint(vars(token))
             pprint(token.split_contents())
         print
-    if True: # with open(path, 'wb') as out:
-        for token in tokens:
-            text = reconstruct(token)
-            if token.token_type == dtbase.TOKEN_BLOCK:
-                t = dtbase.Template(text)
-                nodelist = t.nodelist
-            sys.stdout.write(text)
+    engine = dtengine.Engine.get_default()
+    for token in tokens:
+        text = reconstruct_token(token)
+        if token.token_type == dtbase.TOKEN_BLOCK:
+            command = token.contents.split()[0]
+            if command == 'include':
+                try:
+                    parser = dtbase.Parser([token], engine.template_libraries, 
+                        engine.template_builtins, dtbase.Origin(text))
+                    nodelist = parser.parse()
+                    text = reconstruct_include_node(nodelist[0])
+                except django.template.exceptions.TemplateSyntaxError, e:
+                    # Unbalanced if, elif, etc.  The only blacks we're interested in
+                    # need no balancing.
+                    pass
+        sys.stdout.write(text)
         
 
 
     
-def reconstruct(token):
+def reconstruct_token(token):
     """Given a token, reconstruct the original text."""
     # TOKEN_TEXT = 0
     # TOKEN_VAR = 1
@@ -52,6 +74,11 @@ def reconstruct(token):
     # TOKEN_COMMENT = 3
     formats = ['%s', '{{%s}}', '{%% %s %%}', '{# %s #}']
     return formats[token.token_type] % token.contents 
+
+def reconstruct_include_node(node):
+    extra_context = " ".join(["%s=%s" % (item[0], item[1].token) 
+            for item in node.extra_context.viewitems()])
+    return '{%% include %s %s %%}' % (node.template.token, extra_context)
 
 if __name__ == '__main__':
     main()
