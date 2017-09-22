05d1b636b2f4e656d9f6ccf0de5465ba7417d8ea
Jeffrey Rennie <rennie@google.com>
2017-09-22 09:37:30 -0700

    Lex and reconstruct a whole template.

--- Freeze/freeze.py
+++ Freeze/freeze.py
@@ -1,14 +1,37 @@
 # Hello
 from django.template.base import DebugLexer
+import django.template.base as dtbase
 from pprint import pprint
+import sys
 
 def main():
     s = ('<html>{% if test %}<h1>{{ varvalue }}</h1>{% endif %}\n'
         + '{% include "another_template.html" with a="b" and c="d" %}'
-        +  '</html>')
+        + '</html>\n'
+        + '{# cheesy #}\n'
+        + '{% comment %}\n'
+        + 'You need a new hairstyle.\n'
+        + '{% endcomment %}\n')
+    path = sys.argv[1]
+    s = open(path, 'rb').read()
     lexer = DebugLexer(s)
-    for token in lexer.tokenize():
-        pprint(vars(token))
+    tokens = lexer.tokenize()
+    if False:
+        for token in tokens:
+            pprint(vars(token))
+            pprint(token.split_contents())
+        print
+    open(path, 'wb').write(''.join([reconstruct(token) for token in tokens]))
+
+
+def reconstruct(token):
+    functions = {
+        dtbase.TOKEN_TEXT: lambda token: token.contents,
+        dtbase.TOKEN_VAR: lambda token: '{{%s}}' % token.contents,
+        dtbase.TOKEN_BLOCK: lambda token: '{%% %s %%}' % token.contents,
+        dtbase.TOKEN_COMMENT: lambda token: '{# %s #}' % token.contents
+    }
+    return functions[token.token_type](token)
 
 if __name__ == '__main__':
     main()
