fbfadf9ce76f1e8b6f3ca68b5d68966aa5499d85
Jeffrey Rennie <rennie@google.com>
2017-10-18 12:51:23 -0700

    Report whether test is running as an administrator.

--- kokoro-codelab/continuous.bat
+++ kokoro-codelab/continuous.bat
@@ -1,5 +1,11 @@
 set PATH=C:\Program Files\Java\jdk1.7.0_75\bin;%PATH%
 
+AT > NUL
+IF %ERRORLEVEL% EQU 0 (
+    ECHO you are Administrator
+) ELSE (
+    ECHO you are NOT Administrator. Exiting...
+) 
 dir
 cd github\scratch\kokoro-codelab
 dir
