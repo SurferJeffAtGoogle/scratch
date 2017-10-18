b7562dceba2fb5a9a53f7fc9b923f7bd204fce48
Jeffrey Rennie <rennie@google.com>
2017-10-18 13:02:32 -0700

    WIP

--- kokoro-codelab/continuous.bat
+++ kokoro-codelab/continuous.bat
@@ -1,14 +1,8 @@
 set PATH=C:\Program Files\Java\jdk1.7.0_75\bin;%PATH%
 
-AT > NUL
-IF %ERRORLEVEL% EQU 0 (
-    ECHO you are Administrator
-) ELSE (
-    ECHO you are NOT Administrator. Exiting...
-) 
-dir
+choco install -y microsoft-build-tools dotnetcore-sdk
+
 cd github\scratch\kokoro-codelab
-dir
 call build.bat
 
 exit %ERRORLEVEL%
