fc791f1e9bfd1232d9dda3aed5bb4135450f39e4
Jeffrey Rennie <rennie@google.com>
2018-10-03 14:00:43 -0700

    My link handler is never called.

--- BrokenLink/main.py
+++ BrokenLink/main.py
@@ -1,7 +1,13 @@
 import mistune
 
-renderer = mistune.Renderer(escape=True, hard_wrap=True)
+class LinkCheckingRenderer(mistune.Renderer):
+    def link(self, link, title, content):
+        print(link)
+
+renderer = LinkCheckingRenderer(escape=True, hard_wrap=True)
 markdown = mistune.Markdown(renderer=renderer)
-text = 'I am using **mistune markdown parser**'
+text = '''I am using **mistune markdown parser**.
+(Google)[https://www.google.com/]
+'''
 result = markdown(text)
 print(result)
