9c98541ed8dacc009ed89d32c10042657f274a9d
Jeffrey Rennie <rennie@google.com>
2018-11-27 14:38:50 -0800

    The full version.

--- FindIp/Get-GcpInstance.ps1
+++ FindIp/Get-GcpInstance.ps1
@@ -1,12 +1,32 @@
-if ($false) {
+# Copyright(c) 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+<#
+.SYNOPSIS
+    Given an IP address, finds a GCP Compute instance with the ip address.
+.EXAMPLE
+    PS C:\> .\Get-GcpInstance.ps1 --IpAddress 1.2.3.4
+.OUTPUTS
+    The GCP instance information.
+#>
+
 Param(
     [string][Parameter(Mandatory=$true)] $IpAddress
 )
-
 $myAccount = (gcloud config list --format=json | ConvertFrom-Json).core.account
 $projectIds = (gcloud projects list --format=json | ConvertFrom-Json).ProjectId
 $projectIdsIOwn = foreach ($projectId in $projectIds) {
-    Write-Status "Inspecting $projectId..."
     $bindings = (gcloud projects get-iam-policy $projectId --format=json | ConvertFrom-Json).bindings
     foreach ($binding in $bindings) {
         if ($binding.role -eq 'roles/owner') {
@@ -17,10 +37,6 @@ $projectIdsIOwn = foreach ($projectId in $projectIds) {
         }
     }
 }
-$projectIdsIOwn
-}
-
-$IpAddress= '35.199.164.200'
 
 function Get-GcpInstance {
     param (
