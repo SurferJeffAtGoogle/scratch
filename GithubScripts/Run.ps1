0988b51b0f314e85de11e2b2379d1703192a3dc5
Jeffrey Rennie <rennie@google.com>
2019-10-04 12:00:08 -0700

    Fix regex so it finds fewer results.
    And fix bug making directory.

--- GithubScripts/Run.ps1
+++ GithubScripts/Run.ps1
@@ -24,7 +24,7 @@ function Search-Repo($repoDir, $outputDir) {
     Push-Location
     try {
         Set-Location $repoDir
-        git log '-GcomputeMetadata[/\]+v1beta' --all -p > `
+        git log '-ScomputeMetadata[/\]+v1beta' --pickaxe-regex --all -p > `
             (Join-Path $outputDir "$($repoName)-computeMetadata-v1beta.log")
         git log '-ScomputeMetadata' --all -p > `
             (Join-Path $outputDir "$($repoName)-computeMetadata-all.log")
@@ -40,7 +40,7 @@ function Clone-Repo($repoCloneUrl, $repoName, $clonesDir) {
         $clonesDir = if (Test-Path $clonesDir) {
             Get-Item $clonesDir
         } else {
-            mkdir $clonesDir
+            New-Item -ItemType "directory" $clonesDir
         }
         Set-Location $clonesDir
         if (Test-Path -Path $repoName) {
@@ -65,7 +65,7 @@ if ($Shard) {
     $outputDir = if (Test-Path SearchResults) {
         Get-Item SearchResults
     } else {
-        mkdir SearchResults
+        New-Item -ItemType "directory" SearchResults
     }
     foreach ($repo in $repos) {
         "Cloning $($repo.name)..." | Write-Host
