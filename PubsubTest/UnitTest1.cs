77e83bbc9f68f481ab37c30686c89a87ee7d4024
Jeffrey Rennie <rennie@google.com>
2020-03-18 16:36:59 -0700

    dispose

--- PubsubTest/UnitTest1.cs
+++ PubsubTest/UnitTest1.cs
@@ -1,30 +1,49 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace PubsubTest
 {
-    public class UnitTest1
+    public class UnitTest1 : IDisposable
     {
-        [Fact]
-public void Test1()
-{
-    var spellChecker = SpellCheckerService.CreateClient();
-    var adictionary = spellChecker.CreateDictionary(
-        "test-" + Guid.NewGuid().ToString());
-    try {
-        var bdictionary = spellChecker.CreateDictionary(
-            "test-" + Guid.NewGuid().ToString());
-        try {
-            Assert.Empty(adictionary);
-            adictionary.AddWord("Jeff");
-            Assert.Contains("Jeff", adictionary);
-        } finally {
-            spellChecker.DeleteDictionary(bdictionary);
+        List<Dictionary> dictionariesToDelete = new List<Dictionary>();
+        public void Dispose() {
+            var spellChecker = SpellCheckerService.CreateClient();
+            foreach (var dictionary in dictionariesToDelete) {
+                try {
+                    spellChecker.DeleteDictionary(dictionary);
+                } catch (Exception e) {
+                    Logging.LogException(e);
+                }
+            }
         }
-    } finally {
-        spellChecker.DeleteDictionary(adictionary);
-    }
-}
+
+        [Fact]
+        public void Test1()
+        {
+            var spellChecker = SpellCheckerService.CreateClient();
+            var adictionary = spellChecker.CreateDictionary(
+                "test-" + Guid.NewGuid().ToString());
+            try
+            {
+                var bdictionary = spellChecker.CreateDictionary(
+                    "test-" + Guid.NewGuid().ToString());
+                try
+                {
+                    Assert.Empty(adictionary);
+                    adictionary.AddWord("Jeff");
+                    Assert.Contains("Jeff", adictionary);
+                }
+                finally
+                {
+                    spellChecker.DeleteDictionary(bdictionary);
+                }
+            }
+            finally
+            {
+                spellChecker.DeleteDictionary(adictionary);
+            }
+        }
 
     }
 }
