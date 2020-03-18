3630d8919d3a758f16b8279918dcb3875bee32dd
Jeffrey Rennie <rennie@google.com>
2020-03-18 16:30:42 -0700

    NestedFinallys

--- PubsubTest/UnitTest1.cs
+++ PubsubTest/UnitTest1.cs
@@ -11,15 +11,18 @@ public void Test1()
     var spellChecker = SpellCheckerService.CreateClient();
     var adictionary = spellChecker.CreateDictionary(
         "test-" + Guid.NewGuid().ToString());
-    var bdictionary = spellChecker.CreateDictionary(
-        "test-" + Guid.NewGuid().ToString());
     try {
-        Assert.Empty(adictionary);
-        adictionary.AddWord("Jeff");
-        Assert.Contains("Jeff", adictionary);
+        var bdictionary = spellChecker.CreateDictionary(
+            "test-" + Guid.NewGuid().ToString());
+        try {
+            Assert.Empty(adictionary);
+            adictionary.AddWord("Jeff");
+            Assert.Contains("Jeff", adictionary);
+        } finally {
+            spellChecker.DeleteDictionary(bdictionary);
+        }
     } finally {
         spellChecker.DeleteDictionary(adictionary);
-        spellChecker.DeleteDictionary(bdictionary);
     }
 }
 
